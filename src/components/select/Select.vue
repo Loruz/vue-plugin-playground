3bf02e479da446d94ff37480ddf6ac4dba8b0db7
Vytautas <vpreiks@gmail.com>
2017-12-22 20:34:36 +0200

    router clean up

---
+++ src/components/select/Select.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>cia selectas</div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped ></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Menu from '../components/HelloWorld'
+import Select from '../components/select/Select'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Menu',
+      component: Menu
+    },
+    {
+      path:'/select',
+      name: 'Select',
+      component: Select
     }
   ]
 })
